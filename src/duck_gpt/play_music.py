ec0a51473a3f9683b2c6c15856c7fe534c758787
Phillipe Joos F <phillipe.joos@ug.uchile.cl>
2023-12-18 17:58:56 -0300

    chipi chiiiii

--- src/duck_gpt/play_music.py
+++ src/duck_gpt/play_music.py
@@ -1,19 +1,27 @@
 import os
 import time
+import threading
+
+# function to kill mpg123 process after a certain time
+def kill_music(seconds):
+    # wait for the specified time
+    time.sleep(seconds)
+
+    # kill the mpg123 process
+    os.system("pkill mpg123")
+
 
 # función para reproducir música desde un archivo con os y mpg123
 # recibe el archivo y el tiempo a reproducir como parámetros
 def play_music(file, seconds):
 
+    # thread para matar el proceso de mpg123
+    t = threading.Thread(target=kill_music, args=(seconds,))
+    t.start()
+
     # reproduce el archivo
     os.system("mpg123 ../media/" + file)
 
-    # espera el tiempo especificado
-    time.sleep(seconds)
-   
-    # detiene la reproducción
-    os.system("killall mpg123")
-
 if __name__ == "__main__":
     # reproducir música
-    play_music("chipi.mp3", 10)
+    play_music("chipi.mp3", 3)
